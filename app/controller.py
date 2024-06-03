ad53eddc00282a261ff6da6876e0eaf1587fe29f
Vladimir Mawla <vladimirmawla2@gmail.com>
2024-06-03 18:57:34 +0300

    Created get user by email function

--- app/controller.py
+++ app/controller.py
@@ -5,3 +5,5 @@ from passlib.context import CryptContext
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
