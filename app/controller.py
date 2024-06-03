881435b92fa5be259c06ca87059170c5eb00a9c3
Vladimir Mawla <vladimirmawla2@gmail.com>
2024-06-03 19:02:04 +0300

    Created create user function

--- app/controller.py
+++ app/controller.py
@@ -2,8 +2,18 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from passlib.context import CryptContext
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    password = pwd_context.hash(user.password)
+    db_user = models.User(username=user.username, email=user.email, password=password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
